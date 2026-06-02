0c1a561cb59e4474a74494018eda0d95676a6bfe
smouj <smouj@local>
2026-06-02 23:51:46 +0200

    fix: remove node_modules and dist from git tracking

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { api } from "./client.js";
-export type { Tazo, TazoListResponse, StatsResponse, CollectionResponse } from "./client.js";
-export { STAT_CONFIG, RARITY_STYLE, FRANCHISE_COLOR, formatTazo, formatBattleCard, statBar, banner, fmt } from "./lib/format.js";
